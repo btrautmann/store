89cc9c8ff82def9ff021b2a9dee84ab00a656d90
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-11-15 20:30:18 -0500

    rename emitDigits

--- example/lib/stores.dart
+++ example/lib/stores.dart
@@ -2,13 +2,13 @@ import 'package:flutter_store/flutter_store.dart';
 import 'package:rx_shared_preferences/rx_shared_preferences.dart';
 
 Store<String, String> stringStore(RxSharedPreferences prefs) {
-  Stream<String> emitDigits() async* {
+  Stream<String> fetchString() async* {
     await Future.delayed(const Duration(seconds: 5));
     yield 'response';
   }
 
   return Store.from(
-    fetch: (key) => emitDigits(),
+    fetch: (_) => fetchString(),
     sourceOfTruth: SourceOfTruth.of(
       read: () => prefs.getStringStream('value').map((event) => event ?? 'empty'),
       write: (value) async => prefs.setString('value', value),
